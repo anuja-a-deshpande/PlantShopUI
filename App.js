77cbf442787645a7119ff83c8ee7fa21ecdf7fbf
Anuja Deshpande <anuja.deshpande@spherity.com>
2021-09-28 10:12:53 +0200

    Drawer Navigator added

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import AppNavigator from './src/navigations/Navigator';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { MyDrawer } from './src/navigations/DrawerNavigator';
 
 export default class App extends React.Component {
   render()
   {
     return (
-   <AppNavigator/>
+      <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
   );}
 }
