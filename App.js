1fe6bbfc1ae6f1d8c6db91500c0f20d616d9fec9
Anuja Deshpande <anuja.deshpande@spherity.com>
2021-09-28 20:21:56 +0200

    Detail Screen added

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ export default class App extends React.Component {
   {
     return (
       <NavigationContainer>
-      <MyDrawer />
+      {/* <MyDrawer /> */}
+      <AppNavigator/>
     </NavigationContainer>
   );}
 }
